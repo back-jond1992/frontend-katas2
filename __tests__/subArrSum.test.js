6ffb5557b9dd1c5e6b4b1e7306dbc2e197eb1e0f
back-jond1992 <b.jond7@outlook.com>
2021-11-16 09:20:41 +0000

    finishes subArrSum kata

--- __tests__/subArrSum.test.js
+++ __tests__/subArrSum.test.js
@@ -2,7 +2,18 @@ const { subArrSum } = require("../subArrSum");
 
 describe("subArrSum()", () => {
   test("empty array returns 0", () => {
-    expect(subArrSum([])).toEqual(0);
+    expect(subArrSum([])).toBe(0);
   });
-  test("array of positive integers returns sum", () => {});
+  test("array of positive integers returns sum", () => {
+    expect(subArrSum([1, 2, 3, 4])).toBe(10);
+  });
+  test("array of negative integers 0", () => {
+    expect(subArrSum([-1, -2, -3, -4])).toBe(0);
+  });
+  test("works with mixed array", () => {
+    expect(subArrSum([-2, 1, -3, 4, -1, 2, 1, -5, 4])).toBe(6);
+  });
+  test("works with mixed array", () => {
+    expect(subArrSum([9, 8, 7, -3, 6, 5, 4, -3, 2, 1])).toBe(36);
+  });
 });
--- subArrSum.js
+++ subArrSum.js
@@ -1,8 +1,20 @@
 // Please do not change the name of this function
 const subArrSum = (array) => {
-  if (array.length === 0) {
-    return 0;
+  if (array.length === 0 || array.every((number) => number < 0)) return 0;
+
+  if (array.every((number) => number > 0)) {
+    return array.reduce((a, b) => a + b);
   }
+
+  let currentMax = 0;
+  let maxSoFar = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    currentMax = Math.max(0, currentMax + array[i]);
+    maxSoFar = Math.max(currentMax, maxSoFar);
+  }
+
+  return maxSoFar;
 };
 
 module.exports = { subArrSum };
