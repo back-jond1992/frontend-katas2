8c66aa86275efcefce93e729a96dd28a7e6efa5a
back-jond1992 <b.jond7@outlook.com>
2021-11-17 09:58:42 +0000

    added sudo code

--- romanNumeralEncoder.js
+++ romanNumeralEncoder.js
@@ -6,3 +6,12 @@ const romanNumeralEncoder = (num) => {
 };
 
 module.exports = { romanNumeralEncoder };
+
+
+// is number equal to 1 
+// is number greater than 1 less than 5
+// is number greater than or equal to 5 less than 10
+
+// return roman numeral ->  minus from num -> put remainder back through function
+
+// example 7 greater than 5 so return v - 2 remainder 
