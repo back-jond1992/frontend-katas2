c445a4959ea7fcd7b88f78663760039565d154ef
back-jond1992 <b.jond7@outlook.com>
2021-11-18 09:37:15 +0000

    finishes roman numberals

--- __tests__/romanNumeralEncoder.test.js
+++ __tests__/romanNumeralEncoder.test.js
@@ -1,17 +1,23 @@
 const { romanNumeralEncoder } = require("../romanNumeralEncoder");
 
 describe("romanNumeralEncoder()", () => {
-  test('none number returns "Not a number"', () => {
-    expect(romanNumeralEncoder("badcase")).toEqual("Not a number");
-  });
   test('converts 1 to "I"', () => {
     expect(romanNumeralEncoder(1)).toEqual("I");
   });
   test('converts 2 to "II"', () => {
     expect(romanNumeralEncoder(2)).toEqual("II");
   });
+  test('converts 3 to "III"', () => {
+    expect(romanNumeralEncoder(3)).toEqual("III");
+  });
   test('converts 4 to "IV"', () => {
     expect(romanNumeralEncoder(4)).toEqual("IV");
+  });
+  test('converts 5 to "V"', () => {
+    expect(romanNumeralEncoder(5)).toEqual("V");
+  });
+  test('converts 9 to "IX"', () => {
+    expect(romanNumeralEncoder(9)).toEqual("IX");
   });
   test('converts 10 to "X"', () => {
     expect(romanNumeralEncoder(10)).toEqual("X");
@@ -19,13 +25,40 @@ describe("romanNumeralEncoder()", () => {
   test('converts 17 to "XVII"', () => {
     expect(romanNumeralEncoder(17)).toEqual("XVII");
   });
+  test('converts 20 to "XX"', () => {
+    expect(romanNumeralEncoder(20)).toEqual("XX");
+  });
+  test('converts 29 to "XXIX"', () => {
+    expect(romanNumeralEncoder(29)).toEqual("XXIX");
+  });
+  test('converts 40 to "XL"', () => {
+    expect(romanNumeralEncoder(40)).toEqual("XL");
+  });
+  test('converts 43 to "XLIII"', () => {
+    expect(romanNumeralEncoder(43)).toEqual("XLIII");
+  });
+  test('converts 59 to "L"', () => {
+    expect(romanNumeralEncoder(49)).toEqual("XLIX");
+  });
+  test('converts 50 to "L"', () => {
+    expect(romanNumeralEncoder(50)).toEqual("L");
+  });
   test('converts 60 to "LX"', () => {
     expect(romanNumeralEncoder(60)).toEqual("LX");
   });
   test('converts 75 to "LXXV"', () => {
     expect(romanNumeralEncoder(75)).toEqual("LXXV");
   });
-  test('converts 91 to "XCI"', () => {
-    expect(romanNumeralEncoder(91)).toEqual("XCI");
+  test('converts 88 to "LXXXVIII"', () => {
+    expect(romanNumeralEncoder(88)).toEqual("LXXXVIII");
+  });
+  test('converts 90 to "XC"', () => {
+    expect(romanNumeralEncoder(90)).toEqual("XC");
+  });
+  test('converts 92 to "XCII"', () => {
+    expect(romanNumeralEncoder(92)).toEqual("XCII");
+  });
+  test('converts 100 to "C"', () => {
+    expect(romanNumeralEncoder(100)).toEqual("C");
   });
 });
--- romanNumeralEncoder.js
+++ romanNumeralEncoder.js
@@ -1,17 +1,46 @@
 // Please do not change the name of this function
 const romanNumeralEncoder = (num) => {
-  if (typeof num !== Number) return "Not a number";
-
-  const romanNumerals = 
+  if (num < 1) {
+    return "";
+  }
+  if (num >= 100) {
+    return "C" + romanNumeralEncoder(num - 100);
+  }
+  if (num >= 90) {
+    return "XC" + romanNumeralEncoder(num - 90);
+  }
+  if (num >= 60) {
+    return "LX" + romanNumeralEncoder(num - 60);
+  }
+  if (num >= 50) {
+    return "L" + romanNumeralEncoder(num - 50);
+  }
+  if (num >= 40) {
+    return "XL" + romanNumeralEncoder(num - 40);
+  }
+  if (num >= 10) {
+    return "X" + romanNumeralEncoder(num - 10);
+  }
+  if (num >= 9) {
+    return "IX" + romanNumeralEncoder(num - 9);
+  }
+  if (num >= 5) {
+    return "V" + romanNumeralEncoder(num - 5);
+  }
+  if (num >= 4) {
+    return "IV" + romanNumeralEncoder(num - 4);
+  }
+  if (num >= 1) {
+    return "I" + romanNumeralEncoder(num - 1);
+  }
 };
 
 module.exports = { romanNumeralEncoder };
 
-
-// is number equal to 1 
+// is number equal to 1
 // is number greater than 1 less than 5
 // is number greater than or equal to 5 less than 10
 
 // return roman numeral ->  minus from num -> put remainder back through function
 
-// example 7 greater than 5 so return v - 2 remainder 
+// example 7 greater than 5 so return v - 2 remainder
